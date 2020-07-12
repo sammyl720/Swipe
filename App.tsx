51e5feb5b8b8e672e1bc23ca463ebf04170a31e0
sam leider <sam.leider@hotmail.com>
2020-07-12 02:18:52 -0400

    finished project

--- App.tsx
+++ App.tsx
@@ -1,21 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Deck from './src/components/Deck.tsx';
+import DATA, { card } from './data.ts'
+import { Card, Button } from 'react-native-elements'
 export default function App() {
+
+  const renderCard = (item: card) => {
+    return (
+      <Card
+        key={item.id}
+        title={item.text}
+        image={{ uri: item.uri }}
+      >
+        <Text style={{ marginBottom: 10 }}> I can customize the card further</Text>
+        <Button
+          icon={{ name: 'code'}}
+          backgroundColor='#03a9f4'
+          title='View Now!'
+        />
+      </Card>
+    )
+  }
+
+  const renderNoMoreCards: React.FC = () => {
+    return (
+      <Card title='All Done!'>
+        <Text style={{ marginBottom: 10}}>
+          There's no more content here.
+        </Text>
+        <Button backgroundColor='#03a9f4' title='Get More' />
+      </Card>
+
+    )
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Deck 
+        data={DATA}
+        renderCard={renderCard}
+        onSwipeRight={(item: card ) => {
+          console.log(item.text)
+        }}
+        onSwipeLeft={(item: card ) => {
+          console.log(item.text)
+        }}
+        renderNoMoreCards={renderNoMoreCards}
+      />
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    marginTop: 60
+  }
+})
--- src/components/Deck.tsx
+++ src/components/Deck.tsx
@@ -1,24 +1,41 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { 
   StyleSheet,
   Animated,
   Text,
-  useWindowDimensions,
+  LayoutAnimation,
+  UIManager,
+  Dimensions,
   View,
 PanResponder } from 'react-native'
 import { card } from '../../data'
 import { Gesture } from '../types/gesture.ts'
 
-interface props {
+type props = {
   data: card[],
-  renderCard: React.FC<card>
+  renderCard: React.FC<card>,
+  onSwipeRight: (item: card) => void,
+  onSwipeLeft: (item: card) => void,
+  renderNoMoreCards: () => React.Node
 }
 
-const Deck = ({ data, renderCard}: props) => {
+const SCREEN_WIDTH = Dimensions.get('window').width
+const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH
+const SWIPE_OUT_DURATION = 250
+
+const Deck = ({ data, renderCard, onSwipeRight, onSwipeLeft, renderNoMoreCards}: props) => {
+  const [index, setIndex] = useState(0)
   const position = new Animated.ValueXY()
-  const SCREEN_WIDTH = useWindowDimensions().width
-  const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH
-  const panResponder = useRef(PanResponder.create({
+
+  useEffect(() => {
+    UIManager.setLayoutAnimationEnabledExperimental && UIManager.setLayoutAnimationEnabledExperimental(true)
+    LayoutAnimation.spring()
+  }, [index])
+
+  useEffect(() => {
+    setIndex(0)
+  }, [data])
+  const panResponder = PanResponder.create({
     // called any time user touches screen
     // if function return true the pan responder will track the gesture
     onStartShouldSetPanResponder: (event) => true,
@@ -27,24 +44,49 @@ const Deck = ({ data, renderCard}: props) => {
     // args: event, gesture (has info on type of gesture ie: speed, direction)
     onPanResponderMove: (event, gesture: Gesture ) => {
       // console.log(gesture)
+      // console.log('moving', index)
       position.setValue({ x: gesture.dx, y: gesture.dy })
     },
 
     // called when user releases their finger from screen
     onPanResponderRelease: (event, gesture: Gesture) => {
     if(gesture.dx > SWIPE_THRESHOLD){
-      console.log('Swipe right')
+      // console.log('Swipe right')
+      forceSwipe('right')
     } else if (gesture.dx < - SWIPE_THRESHOLD) {
-      console.log('Swipe Left')
+      // console.log('Swipe Left')
+      forceSwipe('left')
     } else {
       resetPosition()
     }
     }
-  })).current
+  })
 
+  type Direction = 'right' | 'left'
+  const forceSwipe = (direction: Direction ) => {
+    const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH
+    Animated.timing(position, {
+      toValue: {
+        x,
+        y: 0
+      },
+      duration: SWIPE_OUT_DURATION,
+      useNativeDriver: false
+    }).start(onSwipeComplete(direction))
+  }
+
+
+
+  const onSwipeComplete = (direction: Direction ) => {
+    const item = data[index]
+    direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item)
+    position.setValue({ x: 0, y: 0})
+    setIndex(index + 1)
+  }
   const resetPosition = () => {
     Animated.spring(position, {
-      toValue: { x: 0, y: 0}
+      toValue: { x: 0, y: 0},
+      useNativeDriver: false
     }).start()
   }
   const getCardsStyle = () => {
@@ -58,18 +100,31 @@ const Deck = ({ data, renderCard}: props) => {
     }
   }
   const renderCards = () => {
-    return data.map((item: card, index) => {
-      if (index === 0) {
+    if (index >= data.length) {
+      return renderNoMoreCards()
+    }
+    return data.map((item: card, i) => {
+      if ( i < index) {
+        // console.log(`${i} < ${index}`)
+        return null
+      }
+      if (index === i) {
         return (
           <Animated.View key={item.id}
-          style={getCardsStyle()} {...panResponder.panHandlers}
+          style={[getCardsStyle(), styles.cardStyle, { zIndex: 100 }]} {...panResponder.panHandlers}
           >
             { renderCard(item)}
           </Animated.View>
         )
       }
-      return renderCard(item)
-    })
+      return (
+        <Animated.View 
+          key={item.id} 
+          style={[styles.cardStyle, { top: 10 * (i - index), zIndex: 5 }]}>
+          {renderCard(item)}
+        </Animated.View>
+      )
+    }).reverse()
   } 
   return (
     <View>
@@ -78,6 +133,18 @@ const Deck = ({ data, renderCard}: props) => {
   )
 }
 
+Deck.defaultProps = {
+  onSwipeRight: () => {
+    console.log('default prop')
+  },
+  onSwipeLeft: () => {},
+}
+
+const styles = {
+  cardStyle: {
+    position: 'absolute',
+    width: SCREEN_WIDTH
+  }
+};
 export default Deck
 
-const styles = StyleSheet.create({})
